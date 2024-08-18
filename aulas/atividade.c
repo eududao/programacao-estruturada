91fb5f66c375cee1e8e57c5c3a1a46982a7a4176
Eduarda Veloso <169167842+eududao@users.noreply.github.com>
2024-08-18 16:21:49 +0000

    atividade 1

--- aulas/atividade.c
+++ aulas/atividade.c
@@ -10,23 +10,21 @@ void imprimirnumeros() {
     }
 }
 
-
 void somatorioimpares() {
     int soma = 0; 
-    for (int i = 11; i < 1000; i+= 2) {
-        soma+= i;    
+    for (int i = 11; i < 1000; i += 2) {
+        soma += i;    
     }
     printf("%d\n", soma);
 }
 
-
 void imprimirmultiplosde3() {
-    for (int i = 3; i <= 200; i+= 3) {
+    for (int i = 3; i <= 200; i += 3) {
         if (i % 10 != 3) {
-            if ( i == 198 ) {
+            if (i == 198) {
                 printf("%d\n", i);
             } else {
-                printf("%d, " ,  i);
+                printf("%d, ", i);
             }
         }
     }
@@ -35,12 +33,9 @@ void imprimirmultiplosde3() {
 int main() {
     printf("Números de 1 a 100: ");
     imprimirnumeros();
-
     printf("Somatório dos ímpares entre 10 e 1000: ");
     somatorioimpares();
-
     printf("Múltiplos de 3 que não terminam em 3 entre 1 e 200: ");
     imprimirmultiplosde3();
-
     return 0;
 }
